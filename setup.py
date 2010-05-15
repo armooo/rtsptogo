04dc667d1cfd390920cfea32202f795d00815eaf
Jason Michalski <armooo@armooo.net>
2010-05-14 20:07:52 -0400

    Revert "Added setuptools to the requires"
    This reverts commit 197b5d05385e8e031385be1db99e2ac8cac08383.
    
    This was not not needed it was a bug in the ubuntu python-pip
    package.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     version = '0.1',
     packages = find_packages(),
     include_package_data=True,
-    install_requires=['web.py', 'setuptools'],
+    install_requires=['web.py'],
     setup_requires=['setuptools-git'],
     entry_points = {
         'console_scripts' : ['rtsptogo = rtsptogo.main:main',]
