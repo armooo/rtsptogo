551c187874806e8f4fabddf3494e6ac017400708
Jason Michalski <armooo@armooo.net>
2010-05-14 23:21:21 -0400

    Played with setup.py a bit more

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup, find_packages
+from version import get_git_version
 setup(
     name = 'rtsptogo',
-    version = '0.1.1',
+    version = get_git_version(),
     description = 'rtsptogo is a tivo togo proxy serving mp4 video over RSTP',
     long_description=open('README').read(),
     keywords = 'tivo rstp mobile',
@@ -11,9 +12,17 @@ setup(
     license = 'GPL',
     packages = find_packages(),
     include_package_data = True,
+    package_data = { '' : ['RELEASE-VERSION'] },
     install_requires = ['web.py'],
     setup_requires = ['setuptools-git'],
     entry_points = {
-        'console_scripts' : ['rtsptogo = rtsptogo.main:main',]
+        'console_scripts' : ['rtsptogo = rtsptogo.main:main']
     },
+    classifiers = [
+        'Programming Language :: Python',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Topic :: Internet',
+        'Topic :: Multimedia :: Video :: Conversion',
+    ]
 )
