fa91a0671a68ddf51515789c53cde27a2b4dc0bf
Jason Michalski <armooo@armooo.net>
2010-05-14 22:35:42 -0400

    Updated setup.py and added a license

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 from setuptools import setup, find_packages
 setup(
     name = 'rtsptogo',
-    version = '0.1',
+    version = '0.1.1',
+    description = 'rtsptogo is a tivo togo proxy serving mp4 video over RSTP',
+    long_description=open('README').read(),
+    keywords = 'tivo rstp mobile',
+    author = 'Jason Michalski',
+    author_email = 'armooo@armooo.net',
+    url = 'http://github.com/armooo/rtsptogo/',
+    license = 'GPL',
     packages = find_packages(),
-    include_package_data=True,
-    install_requires=['web.py'],
-    setup_requires=['setuptools-git'],
+    include_package_data = True,
+    install_requires = ['web.py'],
+    setup_requires = ['setuptools-git'],
     entry_points = {
         'console_scripts' : ['rtsptogo = rtsptogo.main:main',]
     },
